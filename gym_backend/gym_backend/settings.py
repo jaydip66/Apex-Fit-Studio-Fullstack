d4557f5b7b509f4a2d5fbfc789b40453003bc9e7
jaydip66 <jaydipkulkarni66@gmail.com>
2026-08-19 20:17:58 +0530

    Switch to sqlite3 for cloud deployment

--- gym_backend/gym_backend/settings.py
+++ gym_backend/gym_backend/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'gym_backend.wsgi.application'
 # MySQL Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gym_db',
-        'USER': 'root',        
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 CORS_ALLOW_ALL_ORIGINS = True
